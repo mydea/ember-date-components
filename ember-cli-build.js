0343f53b0c7473654a6ba8b10818f02d2262576d
Francesco Novy <francesco@cropster.com>
2016-05-25 16:27:24 -0700

    Refactor sass partial to ember-date-components/addon

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,7 @@ var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
+    sassOptions: {},
     'ember-bootstrap': {
       'importBootstrapTheme': true
     },
