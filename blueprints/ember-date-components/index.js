2892f5a96334c55a8381fe94a1e980121f1bd980
Francesco Novy <francesco@cropster.com>
2017-04-12 14:09:44 +0200

    Add blueprint to add ember-basic-dropdown style dependency

---
+++ blueprints/ember-date-components/index.js
@@ -0,0 +1,39 @@
+/* eslint-env node */
+'use strict';
+
+const path = require('path');
+const fs = require('fs');
+
+module.exports = {
+  normalizeEntityName() {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
+  },
+
+  afterInstall() {
+    let dependencies = this.project.dependencies();
+    let type;
+    let importStatement = "\n@import 'ember-basic-dropdown';\n";
+
+    if ('ember-cli-sass' in dependencies) {
+      type = 'scss';
+    }
+
+    if (type) {
+      let stylePath = path.join('app', 'styles');
+      let file = path.join(stylePath, `app.${type}`);
+
+      if (!fs.existsSync(stylePath)) {
+        fs.mkdirSync(stylePath);
+      }
+      if (fs.existsSync(file)) {
+        this.ui.writeLine(`Added import statement to ${file}`);
+        return this.insertIntoFile(file, importStatement, {});
+      } else {
+        fs.writeFileSync(file, importStatement);
+        this.ui.writeLine(`Created ${file}`);
+      }
+    }
+  }
+};
