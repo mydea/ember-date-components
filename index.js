ceeaf0184737842836284db1c7f2866f2492616f
Francesco Novy <francesco@cropster.com>
2018-08-10 08:41:13 +0200

    Ensure ember-basic-dropdown is properly invoked

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ module.exports = {
     if (includeCSS) {
       app.import('vendor/ember-date-components.css');
     }
+  },
+
+  contentFor(type, config) {
+    let emberBasicDropdown = this.addons.find((a) => a.name === 'ember-basic-dropdown');
+    return emberBasicDropdown.contentFor(type, config);
   }
 };
